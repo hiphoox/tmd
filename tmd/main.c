91eaf7080f2448fbb0223e3092b049810fab9afc
Norberto Ortigoza <norberto.ortigoza@raku.mx>
2012-10-08 23:55:11 -0500

    Initial Commit

---
+++ tmd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tmd
+//
+//  Created by Norberto Ortigoza on 10/8/12.
+//  Copyright (c) 2012 Norberto Ortigoza. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
